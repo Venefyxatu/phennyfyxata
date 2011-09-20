ffad22cef7ab6fddd6e1f2a8f2bcbb52ea5ec264
Erik Heeren <venefyxatu+git@gmail.com>
2011-09-20 16:30:59 +0200

    Initial checkin for spaghetti module

---
+++ phenny/spaghetti.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+# coding=utf-8
+
+import time
+
+def spaghetti(phenny, input): 
+    """
+    Spaghetti!
+    """
+    phenny.say("Spaghetti! :o")
+
+
+spaghetti.rule = r'.*spaghetti.*'
+
+if __name__ == '__main__': 
+    print __doc__.strip()
