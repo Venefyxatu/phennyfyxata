18da16abbd5e1f43a2aeca17ebfd3e9d31b684f0
Erik Heeren <venefyxatu+git@gmail.com>
2013-01-18 22:41:46 +0100

    Cleanup ugly indentation

--- phennyfyxata/scores/tests.py
+++ phennyfyxata/scores/tests.py
@@ -20,101 +20,101 @@ class ParticipationHelper:
 
 
 class ParticipantTests(TestCase):
-        def setUp(self):
-            starttime = datetime.datetime.now() + datetime.timedelta(0, seconds=300)
-            endtime = starttime + datetime.timedelta(0, seconds=600)
+    def setUp(self):
+        starttime = datetime.datetime.now() + datetime.timedelta(0, seconds=300)
+        endtime = starttime + datetime.timedelta(0, seconds=600)
 
-            self.ph = ParticipationHelper()
+        self.ph = ParticipationHelper()
 
-            self.war = War(starttime=starttime, endtime=endtime)
-            self.war.save()
-            self.writer = Writer(nick='TestWriter')
-            self.writer.save()
-            self.c = Client()
+        self.war = War(starttime=starttime, endtime=endtime)
+        self.war.save()
+        self.writer = Writer(nick='TestWriter')
+        self.writer.save()
+        self.c = Client()
 
-        def test_participate_war(self):
-            self.ph.participate(self.war.id, self.writer.nick)
+    def test_participate_war(self):
+        self.ph.participate(self.war.id, self.writer.nick)
 
-            participants = WarParticipants.objects.filter(war__id=self.war.id)
-            assert len(participants) == 1, 'Should have 1 participant, not %s' % len(participants)
+        participants = WarParticipants.objects.filter(war__id=self.war.id)
+        assert len(participants) == 1, 'Should have 1 participant, not %s' % len(participants)
 
-        def test_withdraw_war(self):
-            self.ph.participate(self.war.id, self.writer.nick)
+    def test_withdraw_war(self):
+        self.ph.participate(self.war.id, self.writer.nick)
 
-            participants = WarParticipants.objects.filter(war__id=self.war.id)
-            assert len(participants) == 1, 'Should have 1 participant, not %s' % len(participants)
+        participants = WarParticipants.objects.filter(war__id=self.war.id)
+        assert len(participants) == 1, 'Should have 1 participant, not %s' % len(participants)
 
-            self.ph.withdraw(self.war.id, self.writer.nick)
+        self.ph.withdraw(self.war.id, self.writer.nick)
 
-            participants = WarParticipants.objects.filter(war__id=self.war.id)
-            assert len(participants) == 0, 'Should have 0 participants, not %s' % len(participants)
+        participants = WarParticipants.objects.filter(war__id=self.war.id)
+        assert len(participants) == 0, 'Should have 0 participants, not %s' % len(participants)
 
-        def test_add_new_participant(self):
-            self.ph.participate(self.war.id, 'NewWriter')
+    def test_add_new_participant(self):
+        self.ph.participate(self.war.id, 'NewWriter')
 
-            participants = WarParticipants.objects.filter(war__id=self.war.id)
-            assert len(participants) == 1, 'Should have 1 participant, not %s' % len(participants)
+        participants = WarParticipants.objects.filter(war__id=self.war.id)
+        assert len(participants) == 1, 'Should have 1 participant, not %s' % len(participants)
 
-        def test_add_same_participant(self):
-            self.ph.participate(self.war.id, self.writer.nick)
+    def test_add_same_participant(self):
+        self.ph.participate(self.war.id, self.writer.nick)
 
-            participants = WarParticipants.objects.filter(war__id=self.war.id)
-            assert len(participants) == 1, 'Should have 1 participant, not %s' % len(participants)
+        participants = WarParticipants.objects.filter(war__id=self.war.id)
+        assert len(participants) == 1, 'Should have 1 participant, not %s' % len(participants)
 
-            self.ph.participate(self.war.id, self.writer.nick)
+        self.ph.participate(self.war.id, self.writer.nick)
 
-            participants = WarParticipants.objects.filter(war__id=self.war.id)
-            assert len(participants) == 1, 'Should have 1 participant, not %s' % len(participants)
+        participants = WarParticipants.objects.filter(war__id=self.war.id)
+        assert len(participants) == 1, 'Should have 1 participant, not %s' % len(participants)
 
-        def test_withdraw_unparticipating_writer(self):
-            self.ph.withdraw(self.war.id, 'NewWriter')
+    def test_withdraw_unparticipating_writer(self):
+        self.ph.withdraw(self.war.id, 'NewWriter')
 
-            participants = WarParticipants.objects.filter(war__id=self.war.id)
-            assert len(participants) == 0, 'Should have 0 participants, not %s' % len(participants)
+        participants = WarParticipants.objects.filter(war__id=self.war.id)
+        assert len(participants) == 0, 'Should have 0 participants, not %s' % len(participants)
 
-        def test_participate_nonexistant_war(self):
-            response = self.c.post('/wars/%s/participate/' % 9, {'writer': self.writer.nick})
-            assert response.status_code == 404, 'Response status should be 404, not %s' % response.status_code
+    def test_participate_nonexistant_war(self):
+        response = self.c.post('/api/war/%s/participate/' % 9, {'writer': self.writer.nick})
+        assert response.status_code == 404, 'Response status should be 404, not %s' % response.status_code
 
-        def test_list_participants(self):
-            self.ph.participate(self.war.id, self.writer.nick)
+    def test_list_participants(self):
+        self.ph.participate(self.war.id, self.writer.nick)
 
-            participants = WarParticipants.objects.filter(war__id=self.war.id)
-            assert len(participants) == 1, 'Should have 1 participant, not %s' % len(participants)
+        participants = WarParticipants.objects.filter(war__id=self.war.id)
+        assert len(participants) == 1, 'Should have 1 participant, not %s' % len(participants)
 
-            response = self.c.get('/wars/%s/listparticipants/' % self.war.id)
-            assert response.status_code == 200, 'Response status should be 200, not %s' % response.status_code
-            expected_response = [self.writer.nick]
-            assert json.loads(response.content) == expected_response, 'Response should be "%s", not %s' % (expected_response, json.loads(response.content))
+        response = self.c.get('/api/war/%s/listparticipants/' % self.war.id)
+        assert response.status_code == 200, 'Response status should be 200, not %s' % response.status_code
+        expected_response = [self.writer.nick]
+        assert json.loads(response.content) == expected_response, 'Response should be "%s", not %s' % (expected_response, json.loads(response.content))
 
-            self.ph.participate(self.war.id, 'NewWriter')
+        self.ph.participate(self.war.id, 'NewWriter')
 
-            participants = WarParticipants.objects.filter(war__id=self.war.id)
-            assert len(participants) == 2, 'Should have 1 participant, not %s' % len(participants)
+        participants = WarParticipants.objects.filter(war__id=self.war.id)
+        assert len(participants) == 2, 'Should have 1 participant, not %s' % len(participants)
 
-            response = self.c.get('/wars/%s/listparticipants/' % self.war.id)
-            assert response.status_code == 200, 'Response status should be 200, not %s' % response.status_code
-            expected_response = [self.writer.nick, 'NewWriter']
-            assert json.loads(response.content) == expected_response, 'Response should be "%s", not %s' % (expected_response, json.loads(response.content))
+        response = self.c.get('/api/war/%s/listparticipants/' % self.war.id)
+        assert response.status_code == 200, 'Response status should be 200, not %s' % response.status_code
+        expected_response = [self.writer.nick, 'NewWriter']
+        assert json.loads(response.content) == expected_response, 'Response should be "%s", not %s' % (expected_response, json.loads(response.content))
 
-            self.ph.withdraw(self.war.id, self.writer.nick)
+        self.ph.withdraw(self.war.id, self.writer.nick)
 
-            participants = WarParticipants.objects.filter(war__id=self.war.id)
-            assert len(participants) == 1, 'Should have 1 participant, not %s' % len(participants)
+        participants = WarParticipants.objects.filter(war__id=self.war.id)
+        assert len(participants) == 1, 'Should have 1 participant, not %s' % len(participants)
 
-            response = self.c.get('/wars/%s/listparticipants/' % self.war.id)
-            assert response.status_code == 200, 'Response status should be 200, not %s' % response.status_code
-            expected_response = ['NewWriter']
-            assert json.loads(response.content) == expected_response, 'Response should be "%s", not %s' % (expected_response, json.loads(response.content))
+        response = self.c.get('/api/war/%s/listparticipants/' % self.war.id)
+        assert response.status_code == 200, 'Response status should be 200, not %s' % response.status_code
+        expected_response = ['NewWriter']
+        assert json.loads(response.content) == expected_response, 'Response should be "%s", not %s' % (expected_response, json.loads(response.content))
 
-            self.ph.withdraw(self.war.id, 'NewWriter')
+        self.ph.withdraw(self.war.id, 'NewWriter')
 
-            participants = WarParticipants.objects.filter(war__id=self.war.id)
-            assert len(participants) == 0, 'Should have 0 participants, not %s' % len(participants)
+        participants = WarParticipants.objects.filter(war__id=self.war.id)
+        assert len(participants) == 0, 'Should have 0 participants, not %s' % len(participants)
 
-            response = self.c.get('/wars/%s/listparticipants/' % self.war.id)
-            assert response.status_code == 200, 'Response status should be 200, not %s' % response.status_code
-            assert json.loads(response.content) == [], 'Response should be [], not %s' % json.loads(response.content)
+        response = self.c.get('/api/war/%s/listparticipants/' % self.war.id)
+        assert response.status_code == 200, 'Response status should be 200, not %s' % response.status_code
+        assert json.loads(response.content) == [], 'Response should be [], not %s' % json.loads(response.content)
 
 
 class WarTests(TestCase):
