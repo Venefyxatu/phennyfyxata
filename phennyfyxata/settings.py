2acf2b14082253368c1fa4bcbe3d3cd322dfd63d
Erik Heeren <venefyxatu+git@gmail.com>
2013-04-24 17:40:12 +0200

    Small password template fix to cooperate better with password changing script

--- phennyfyxata/settings.py
+++ phennyfyxata/settings.py
@@ -14,7 +14,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql', # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
         'NAME': 'phenny_scores',                      # Or path to database file if using sqlite3.
         'USER': 'django',                      # Not used with sqlite3.
-        'PASSWORD': 'PUPPET_PHENNY_SCORES_PASSWORD',                  # Not used with sqlite3.
+        'PASSWORD': 'PUPPET__PHENNY__SCORES__PASSWORD',                  # Not used with sqlite3.
         'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
         'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
         'OPTIONS': { 'init_command': 'SET storage_engine=MyISAM', },
