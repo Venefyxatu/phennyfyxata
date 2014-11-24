4c2499a495431eb90b8dbe71879d7db5ba2da17c
Erik Heeren <heerene@b-virtual.org>
2014-11-24 15:28:27 +0100

    She also does pet food now

--- phenny/sandwich.py
+++ phenny/sandwich.py
@@ -6,8 +6,9 @@ Author: Thomas Hirsch, http://relet.net
 
 import random
 
-action = chr(1)+"ACTION "
-afin   = chr(1)
+action = chr(1) + "ACTION "
+afin = chr(1)
+
 
 def sandwich(phenny, input):
     topping = random.choice(('avocado', 'sla', 'tomato', 'mozzarella', 'spek',
@@ -21,11 +22,35 @@ def sandwich(phenny, input):
                              'standaard', 'zand', 'heksen', 'mega-grote',
                              'rookworst', 'usbstick', 'rookworst-usbstick'))
     print '---'
-    phenny.say(action+"biedt "+input.nick+" een lekkere "+topping+" sandwich aan."+afin)
+    phenny.say(action + "biedt " + input.nick + " een lekkere " + topping + " sandwich aan." + afin)
     print '---'
 
-sandwich.commands=["eten","noms"]
-sandwich.priority="low"
+sandwich.commands = ["eten", "noms"]
+sandwich.priority = "low"
+
+FOODS = ['katten', 'honden', 'vissen', 'slangen', 'muizen', 'uilen',
+         'valken', 'collega-', ]
+
+
+def feed(phenny, pet, owner):
+    action = chr(1) + "ACTION "
+
+    randomfood = random.choice(FOODS)
+    phenny.say(action + 'geeft de %s van %s een blik %seten' % (pet,
+                                                                owner,
+                                                                randomfood))
+
+
+def petfood(phenny, input):
+    """
+    Feed a user's pet
+    """
+    pet = input.group(1)
+    owner = input.group(2)
+    feed(phenny, pet, owner)
+
+
+petfood.rule = r'de (.*) van (.*) heeft honger'
 
 if __name__ == '__main__':
-   print __doc__.strip()
+    print __doc__.strip()
